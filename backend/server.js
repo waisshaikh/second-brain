58d857b27cd8c6ec0340069b45421b78262645ae
Wais Shaikh <143809659+waisshaikh@users.noreply.github.com>
2026-04-02 20:02:37 +0530

    Refactor server startup and add test route
    Refactor server initialization to start after DB connection and add a test route.

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,26 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import path from "path";
+import express from "express"; 
 import app from "./src/app.js";
 import connectDB from "./src/config/database.js";
 
-connectDB();
-
-const __dirname = path.resolve();
-
-// SERVE FRONTEND BUILD
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)});
+//  START SERVER ONLY AFTER DB CONNECT
+connectDB()
+  .then(() => {
+    console.log("MongoDB Connected!");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT} `);
+    });
+  })
+  .catch((err) => {
+    console.log("DB ERROR ", err.message);
+  });
+
+//  TEST ROUTE
+app.get("/", (req, res) => {
+  res.send("API Running ");
+});
