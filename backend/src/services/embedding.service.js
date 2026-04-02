bbbac54638ec2800dbe3fec34e4eacbb4056c744
waisshaikh <waisshaikh394@gmail.com>
2026-04-02 18:36:06 +0530

    disabled embedding for deployment

--- backend/src/services/embedding.service.js
+++ backend/src/services/embedding.service.js
@@ -1,18 +1,3 @@
-import axios from "axios";
-
-export const generateEmbedding = async (text) => {
-  try {
-    const response = await axios.post(
-      "http://localhost:11434/api/embeddings",
-      {
-        model: "nomic-embed-text",
-        prompt: text.slice(0, 150), 
-      }
-    );
-
-    return response.data.embedding || [];
-  } catch (error) {
-    console.error("Ollama Error:", error.message);
-    return [];
-  }
+export const generateEmbedding = async () => {
+  return [];
 };
