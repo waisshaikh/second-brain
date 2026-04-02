0dea716ff227afa99ae4d25d78b2c2587a08f180
waisshaikh <waisshaikh394@gmail.com>
2026-04-02 19:50:43 +0530

    Refactor server.js to streamline database connection and serve frontend assets

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,22 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import path from "path";
 import app from "./src/app.js";
 import connectDB from "./src/config/database.js";
 
+connectDB();
+
+const __dirname = path.resolve();
+
+// SERVE FRONTEND BUILD
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
 const PORT = process.env.PORT || 5000;
 
-//  DB connect first
-connectDB()
-  .then(() => {
-    console.log("MongoDB Connected ");
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT} `);
-    });
-  })
-  .catch((err) => {
-    console.log("DB ERROR ❌", err.message);
-  });
-
-//  health route (test ke liye)
-app.get("/", (req, res) => {
-  res.send("API Running ");
-});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)});
